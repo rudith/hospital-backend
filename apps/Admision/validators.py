698d702509406700b7776ebb1fe6df0e94ed4b53
reble291230 <echalcoc@unsa.edu.pe>
2019-10-06 20:20:35 -0500

    Sprint8-Elber-final: validacion de fechas

--- apps/Admision/validators.py
+++ apps/Admision/validators.py
@@ -1,4 +1,6 @@
 from django.core.exceptions import ValidationError
+from datetime import datetime
+from datetime import date
 
 
 def dni(value):
@@ -15,7 +17,8 @@ def dni(value):
 
 def fechaNac(value):
    #if not  len(value) == 8:
-   #a= int((datetime.now().date() - self.fechaNac).days / 365.25)
-   year = int((datetime.now().date() - date(value)).days / 365.25)
-   if not  year > 130 :
-       raise ValidationError('datos de fecha no validos')
+    a= int((datetime.now().date() - value).days / 365.25)
+   #year = int((datetime.now().date() - fechaNac ).days / 365.25)
+    
+    if not  0 < a <130  :
+     raise ValidationError('datos de fecha no validos')
