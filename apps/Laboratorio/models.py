4696632952304ed899bea3f7cef837470b56fad3
Julio <jvicenteg.96@gmail.com>
2019-10-23 21:25:15 -0500

    S11-Avance-Julio: Generacion de Nombre Historia PDF, NOMBRE EN CABECERA REQUERIDO

--- apps/Laboratorio/models.py
+++ apps/Laboratorio/models.py
@@ -9,7 +9,7 @@ class TipoExamen(models.Model):
 
 
 class ExamenLabCab(models.Model):
-    nombre = models.CharField(max_length=100,blank=True)
+    nombre = models.CharField(max_length=100)
     dni = models.CharField(max_length=8)
     tipoExam = models.ForeignKey(TipoExamen, on_delete=models.CASCADE)
     orden = models.CharField(max_length=100, blank=True, null=True)
